6a8a80efa796825452a75a04e4f609b89cf080ef
samwanyua <samexample8@gmail.com>
2024-02-03 04:50:43 +0300

    Blog - added events

--- blog/src/Components/Home.js
+++ blog/src/Components/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log(e)
+    }
+    const handleClickAgain = (student,e) => {
+        console.log('Hello' + student, e.target)
+    }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Warui',e)}>Click student</button>
+
         </div>
      );
 }
