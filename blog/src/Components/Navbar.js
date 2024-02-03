b1e6015124f018a8498b50acefd168d75d5ab557
samwanyua <samexample8@gmail.com>
2024-02-03 17:25:13 +0300

    Blog - route Links

--- blog/src/Components/Navbar.js
+++ blog/src/Components/Navbar.js
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const Navbar = () => {
     return (  
         <nav className="navbar">
             <h1>The Wanyua Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
+                <Link to="/">Home</Link>
+                <Link to="/create">New Blog</Link>
             </div>
         </nav>
     );
