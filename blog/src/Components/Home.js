c8f9a6bce3fbea5897faeb090f8d22316bf8e7e2
samwanyua <samexample8@gmail.com>
2024-02-03 11:45:31 +0300

    Blog - added usestate

--- blog/src/Components/Home.js
+++ blog/src/Components/Home.js
@@ -1,16 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
 
+    const [name, setName] = useState('Mario')
+
     const handleClick = (e) => {
-        console.log(e)
+        setName('Luigi')
     }
-    const handleClickAgain = (student,e) => {
-        console.log('Hello' + student, e.target)
-    }
+    // const handleClickAgain = (student,e) => {
+    //     console.log('Hello' + student, e.target)
+    // }
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name}</p>
             <button onClick={handleClick}>Click me</button>
-            <button onClick={(e) => handleClickAgain('Warui',e)}>Click student</button>
+            {/* <button onClick={(e) => handleClickAgain('Warui',e)}>Click student</button> */}
 
         </div>
      );
