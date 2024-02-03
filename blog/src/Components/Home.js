453b37bb5189a91d44c2d816927c9615ea11e9e0
samwanyua <samexample8@gmail.com>
2024-02-03 12:38:05 +0300

    Blog - functions as props

--- blog/src/Components/Home.js
+++ blog/src/Components/Home.js
@@ -5,14 +5,25 @@ const Home = () => {
     const[blogs, setBlogs] = useState([
         {title: 'My new website', body: 'Lorem ipsum....', author: 'mario', id: 1},
         {title: 'Welcome to Chicago', body: 'Lorem ipsum....', author: 'yoshi', id: 2},
-        {title: 'Top 10 visiting places in Chicago', body: 'Lorem ipsum....', author: 'uiyo', id: 3}
+        {title: 'Top 10 visiting places in Chicago', body: 'Lorem ipsum....', author: 'mario', id: 3}
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id != id)
+        setBlogs(newBlogs)
+    }
     return ( 
         <div className="home">
             <BlogList 
                 blogs = {blogs}
                 title = "All Blogs"
-            
+                handleDelete={handleDelete}
+            />
+            <BlogList 
+                blogs = {blogs.filter((blog) => blog.author === 'mario')}
+                title = "Mario's blogs"
+                handleDelete={handleDelete}
+
             />
         </div>
      );
